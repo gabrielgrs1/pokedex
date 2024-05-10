161865b02df3955c7384cb894e686debc0be81df
Gabriel Guilherme Rodrigues Silva <gabriel_guilherme2006@hotmail.com>
2024-05-10 21:11:30 +0100

    feat: implements empty search

--- app/src/main/java/com/gabrielgrs1/pokedex/presentation/components/EmptySearch.kt
+++ app/src/main/java/com/gabrielgrs1/pokedex/presentation/components/EmptySearch.kt
@@ -1,16 +1,52 @@
 package com.gabrielgrs1.pokedex.presentation.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 @Composable
 fun EmptySearch(modifier: Modifier = Modifier) {
-    /* TODO: Implements emtpy state */
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            imageVector = Icons.Default.Search,
+            contentDescription = "icon search",
+            tint = Color.Black,
+            modifier = Modifier.size(96.dp)
+        )
+        Text(
+            text = "Unfortunately no results were found, try searching for the pokemon's full name",
+            color = Color.Black,
+            style = MaterialTheme.typography.headlineSmall,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(
+                top = 16.dp, start = 16.dp, end = 16.dp
+            )
+        )
+    }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun EmptySearchPrev() {
     EmptySearch()
